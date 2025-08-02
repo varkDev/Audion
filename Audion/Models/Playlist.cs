d5091c88062f2d5d4e57a52a0a954a67fa0145da
qbrex <varkDev@users.noreply.github.com>
2025-08-02 01:57:43 +0100

    Added models for tracks and playlists, used a bit of help from AI, I understand the code fully now and I understand the code I've written

---
+++ Audion/Models/Playlist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+public class Playlist : INotifyPropertyChanged
+{
+    private string name;
+    private ObservableCollection<Track> tracks = new();
+
+    public string Name
+    {
+        get => name; //fetch current value of name
+        set
+        {
+            if (name != value) //if current set value is not equal to the new value
+            {
+                name = value; //change the already set value to the new value
+                OnPropertyChanged(nameof(Name)); //notify the UI that the Name property has changed
+            }
+        }
+    }
+
+    public ObservableCollection<Track> Tracks
+    {
+        get => tracks; //fetch current value of tracks
+        set
+        {
+            if (tracks != value) //if current set value is not equal to the new value
+            {
+                tracks = value; //change the already set value to the new value
+                OnPropertyChanged(nameof(Tracks)); //notify the UI that the Tracks property has changed
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged; //defining the event for the property change
+
+    protected void OnPropertyChanged(string propertyName) //protected means that it can only be accessed within this class
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); //if PropertyChanged is not null, invoke it with the current instance and the property name that has changed
+    }
+
+}
---
+++ Audion/Models/Track.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+public class Track : INotifyPropertyChanged
+{
+    private string filePath;
+    private string title;
+
+    public string FilePath
+    {
+        get => filePath; //fetch current value of filepath
+        set
+        { 
+            if (filePath != value) //if current set value is not equal to the new value
+            {
+                filePath = value; //change the already set value to the new value
+                OnPropertyChanged(nameof(FilePath)); //notify the UI that the FilePath property has changed
+            }
+        }
+    }
+
+    public string Title
+    {
+        get => title; //fetch current value of title
+        set
+        {
+            if (title != value) //if current set value is not equal to the new value
+            {
+                title = value; //change the already set value to the new value
+                OnPropertyChanged(nameof(Title)); //notify the UI that the Title property has changed
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged; //defining the event for the property change
+
+    protected void OnPropertyChanged(string propertyName) //protected means that it can only be accessed within this class
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); //if PropertyChanged is not null, invoke it with the current instance and the property name that has changed
+    }
+
+}
